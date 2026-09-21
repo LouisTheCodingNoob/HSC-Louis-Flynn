743d81123c38ddd7fe00bef14a1b664672dc9f51
Louis Flynn <fiontarfloinn@gmail.com>
2026-09-21 21:26:34 +0100

    Add files via upload

---
+++ HW3/script.js
@@ -0,0 +1,33 @@
+var message = 'Meet the Jack of All Technical Trades';
+
+function updateHeader() {
+	var header = document.get.ElementById('header');
+	header.text.Content = message;
+}
+
+updateHeader();
+
+var message = 'Matthew Hunt, owner of Principled Software Solutions, LLC, founded his software company in 2021 with the goal of becoming a “Jack of all Technical Trades,” delivering comprehensive technical solutions for his clients.';
+
+function updateParagraph() {
+	var paragraph = document.getElementById('paragraph'); 
+	paragraph.text.Content = paragraphMessage;
+}
+
+updateparagraph();
+
+function calculateFarenheit(degreesCelsius) }
+	return degreesCelsius * (9/5) + 32;
+}
+
+var degreesCelsius = 27;
+
+var degreesFarenheit = calculateFarenheit(degreesCelsius);
+
+var area = (function() {
+	var width = 30;
+	var height = 40;
+	return width * height;
+}());
+
+console.log('the area of my house is ' + area + ' sq ft');
